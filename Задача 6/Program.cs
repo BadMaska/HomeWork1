81e171bb429aa8227ac47cbe861e917b86ec5b26
Andrey <andeymaska@gmail.com>
2022-12-02 04:00:42 +0300

    задача 6

--- Задача 6/Program.cs
+++ Задача 6/Program.cs
@@ -6,7 +6,7 @@
 7 -> нет
 */
 
-Console.WriteLine("Введите число : ");
+Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a % 2 == 0)
 Console.WriteLine("Четное");
